dcf004e0352fd89c007ef66529db88afe992ac75
Taylor Rozier <roziertaylor@gmail.com>
2023-07-26 18:33:44 -0700

    wed

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
-// We retrieve a set of the 50 top backgrounds
-// We retrieve a set of the 50 top backgrounds
+
+// Retrieve a set of the 50 top backgrounds
 $.getJSON('http://www.colourlovers.com/api/patterns/top?jsonCallback=?&numResults=50',
           function(data) {
 
-  // we randomly select one of the elements
+  // Randomly select one of the elements
   var element = data[Math.floor(Math.random()*50)];
 
-  // we update the body background with the url
+  // Update the body background with the url
   $('body').css({
     "background-image": "url("+ element.imageUrl +")"
   });
